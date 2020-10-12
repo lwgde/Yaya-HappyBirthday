3f28fec055711174b864b4c0dcf02b126cb5837e
lwgde <43670529+lwgde@users.noreply.github.com>
2020-10-12 13:25:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ var config = {
      */
     imgs: {
         "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        bannar_coming: "装饰一下",
+        balloons_flying: "好像还少了点什么",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
